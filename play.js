794f72021c671c3254dd7bc2d9484f97bd2bc817
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2023-05-16 23:27:11 +0200

    Update play.js

--- play.js
+++ play.js
@@ -1,9 +1,21 @@
-onload=function(){
-var id=location.hash;
-play_frame.src="/play_frame.html"+id;
-  recommended_frame.src="/recommended.html"+id;
+var xhr=new XMLHttpRequest();
+xhr.open("GET","/js_api/games.json",false);
+xhr.send();
+var games=JSON.parse(xhr.responseText);
+onload=onhashchange=function(){
+  var id=location.hash.slice(1);
+  for (i in games){
+  if (games[i].id===id){
+    game=games[i];
+    break;
+  }
+  }
+play_frame.src=game["url"];
+recommended_frame.src="/recommended.html#"+id;
 }
-onhashchange=function(){
-var id=location.hash;
-play_frame.src="/play_frame.html"+id;
-  recommended_frame.src="/recommended.html"+id;}
+
+fullscreen=function(){
+}
+
+home=function(){
+}
