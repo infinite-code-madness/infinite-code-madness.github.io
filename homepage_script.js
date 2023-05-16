38f582cb608097faca646e5b689a6555b59d2519
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2023-05-16 19:52:16 +0200

    Update homepage_script.js

--- homepage_script.js
+++ homepage_script.js
@@ -1,12 +1,4 @@
-var xhr=new XMLHttpRequest();
-xhr.open("GET","/js_api/urls_2.json",false);
-xhr.send();
-var urls=JSON.parse(xhr.responseText)["homepage"];
-for (i in urls){
-  for (i2 in urls[i]){
-    document.getElementById(i).setAttribute(i2,urls[i][i2]);
-  }
-}
+load_links("homepage");
 document.body.style.margin="0px";
 var topsize=download_a.getClientRects()[0].height*1.2;
 onresize=function(){
