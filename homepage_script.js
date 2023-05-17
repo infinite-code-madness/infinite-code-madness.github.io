e472451a09119ae487fe897d615f9d51c6cba939
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2023-05-17 20:30:16 +0200

    Update homepage_script.js

--- homepage_script.js
+++ homepage_script.js
@@ -3,6 +3,5 @@ document.body.style.margin="0px";
 var topsize=download_a.getClientRects()[0].height*1.2;
 onresize=function(){
   homepage_frame.style.width=innerWidth+"px";
-  homepage_frame.style.height=innerHeight-topsize+"px";
 }
 onresize();
