8e9010e45d5e96c4d9d989942bd48efc380754e7
infinite-code-madness <130861756+infinite-code-madness@users.noreply.github.com>
2023-05-17 23:26:13 +0200

    Update homepage_script.js

--- homepage_script.js
+++ homepage_script.js
@@ -1,7 +1,24 @@
 load_links("homepage");
 document.body.style.margin="0px";
 var topsize=download_a.getClientRects()[0].height*1.2;
+window.__defineGetter__("scrollbarWidth",function(){
+  var outer=document.createElement('div');
+  outer.style.visibility='hidden';
+  outer.style.overflow='scroll';
+  document.body.appendChild(outer);
+
+  var inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  var w=(outer.offsetWidth-inner.offsetWidth);
+
+  document.body.removeChild(outer);
+
+  return w;
+});
+window.__defineGetter__("isScrollbarVisible",function(){
+return document.body.getClientRects()[0].height>innerHeight});
 onresize=function(){
-  homepage_frame.style.width=innerWidth+"px";
+  homepage_frame.style.width=(innerWidth-(isScrollbarVisible?scrollbarWidth:0))+"px";
 }
 onresize();
